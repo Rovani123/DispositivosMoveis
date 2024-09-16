f809c8626d27e07686960cd8fc35864ca4ff1149
Lucas <Lucas.r2006@aluno.ifsc.edu.br>
2024-09-16 10:18:36 -0300

    2

---
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    int num1;
+    int num2;
+    Button button;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btClick);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Random random= new Random();
+
+                Button b=(Button) view;
+                TextView t1 =(TextView) view;
+                TextView t2 =(TextView) view;
+                TextView th =(TextView) view;
+
+                t1 = findViewById(R.id.txtInicial);
+                t2 = findViewById(R.id.txtFinal);
+                th = findViewById(R.id.txtHello);
+
+                num1 = Integer.parseInt(String.valueOf(t1.getText()));
+                num2 = Integer.parseInt(String.valueOf(t2.getText()));
+
+                th.setText(String.valueOf(random.nextInt(num2-num1)+num1));
+            }
+        });
+
+    }
+}
