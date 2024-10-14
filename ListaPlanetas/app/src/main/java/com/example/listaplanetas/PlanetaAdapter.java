b4e2b0aca3bf83b89fd10894e7f75d5b95fbe649
Jesus <lucas.r2006@aluno.ifsc.edu.br>
2024-10-14 10:50:47 -0300

    toma donnovan

--- ListaPlanetas/app/src/main/java/com/example/listaplanetas/PlanetaAdapter.java
+++ ListaPlanetas/app/src/main/java/com/example/listaplanetas/PlanetaAdapter.java
@@ -1,24 +1,23 @@
 package com.example.listaplanetas;
 
 import android.content.Context;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PlanetaAdapter extends ArrayAdapter<Planeta> {
 
     Context mContexto;
     Integer mRes;
-    List mListaPlaneta;
+    List<Planeta> mListaPlaneta;
 
     public PlanetaAdapter(@NonNull Context context, int resource, @NonNull List<Planeta> objects) {
         super(context, resource, objects);
@@ -31,6 +30,12 @@ public class PlanetaAdapter extends ArrayAdapter<Planeta> {
         LayoutInflater layoutInflater = LayoutInflater.from(mContexto);
         View v = layoutInflater.inflate(mRes,parent,false);
 
+        TextView tvnomeplaneta = v.findViewById(R.id.lblnome);
+        ImageView imageview = v.findViewById(R.id.imageview);
+
+        Planeta planeta = mListaPlaneta.get(position);
+        tvnomeplaneta.setText(planeta.nome);
+        imageview.setImageResource(planeta.foto);
 
         return v;
     }
--- ListaPlanetas/app/src/main/java/com/example/listaplanetas/PlanetaDAO.java
+++ ListaPlanetas/app/src/main/java/com/example/listaplanetas/PlanetaDAO.java
@@ -5,12 +5,7 @@ import java.util.ArrayList;
 public class PlanetaDAO {
     ArrayList<Planeta> planetas;
 
-    public PlanetaDAO() {
-        planetas = new ArrayList<>();
-        planetas.add(new Planeta("Mercurio",R.drawable.mercury));
-        planetas.add(new Planeta("Venus",R.drawable.venus));
-        planetas.add(new Planeta("Terra",R.drawable.earth));
-    }
+    
     public ArrayList<Planeta> getPlanetas() {
         return planetas;
     }
