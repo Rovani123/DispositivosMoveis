ebd2b77f27abbe6fcd67070c6a8d628fa47d2995
Jesus <lucas.r2006@aluno.ifsc.edu.br>
2024-10-21 11:24:11 -0300

    "banco de dados" git " " ; fjivsjgm[ pḱltg0ij/ V[FǴ

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,38 +3,44 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.Date;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase banco;
-    EditText editNome, editDataNsc, editEmail;
+    EditText txtNome, txtDataNsc, txtEmail;
     Button bt;
+    ListView list;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editNome = findViewById(R.id.txtnome);
-        editEmail= findViewById(R.id.txtemail);
-        editDataNsc = findViewById(R.id.txtdata);
+        txtNome = findViewById(R.id.txtnome);
+        txtEmail = findViewById(R.id.txtemail);
+        txtDataNsc = findViewById(R.id.txtdata);
         bt=findViewById(R.id.button);
+        list = findViewById(R.id.listview);
 
         banco = openOrCreateDatabase("banco",MODE_PRIVATE, null);
         banco.execSQL("CREATE TABLE if not exists pessoas (id  INTEGER PRIMARY KEY autoincrement,nome varchar, email varchar, dtnsc date)");
 
         bt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-            String nome = editNome.getText().toString();
-            String email = editEmail.getText().toString();
-            String data = editDataNsc.getText().toString();
+            String nome = txtNome.getText().toString();
+            String email = txtEmail.getText().toString();
+            String data = txtDataNsc.getText().toString();
 
             ContentValues cv = new ContentValues();
             cv.put("nome",nome);
@@ -47,8 +53,23 @@ public class MainActivity extends AppCompatActivity {
             } else{
                 Toast.makeText(MainActivity.this, "Garr", Toast.LENGTH_SHORT).show();
             }
-
+                carregarlist();
             }
         });
+        carregarlist();
     }
+
+    public void carregarlist(){
+        Cursor cursor =banco.rawQuery("SELECT * FROM pessoas",null);
+        cursor.moveToFirst();
+        ArrayList<String> nomes = new ArrayList<>();
+        while(!cursor.isAfterLast())
+        {
+            nomes.add(cursor.getString(1));
+            cursor.moveToNext();
+        }
+        ArrayAdapter<String> adap = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1,nomes);
+        list.setAdapter(adap);
+    }
+
 }
